6d76ab4e70e8d6f6f82f05cbe5d9879552e510bf
Surya <suryavelu2112@gmail.com>
2022-11-20 00:08:06 +0530

    fina

---
+++ Final Deliverables/Code/sgdemo.py
@@ -0,0 +1,17 @@
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+message = Mail(
+    from_email='works.suryav@gmail.com',
+    to_emails='suryavelu2112@gmail.com',
+    subject='Inventory Stocks running Low !!',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+try:
+    sg = SendGridAPIClient("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")
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
